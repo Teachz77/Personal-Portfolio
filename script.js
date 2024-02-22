147cde7dbdc6e2c756236c3f52a9d72c5b3da2b2
Teachz77 <teachhatz96@gmail.com>
2024-02-22 22:30:55 +0700

    add reveal animation

---
+++ script.js
@@ -0,0 +1,10 @@
+ScrollReveal({
+    reset: true,
+    distance: '60px',
+    duration: 2500,
+    delay: 400
+});
+
+ScrollReveal().reveal('.left-wrap, .des-tech', { delay: 500, origin: 'left' });
+ScrollReveal().reveal('.illustration, .img-tech', { delay: 500, origin: 'bottom' });
+ScrollReveal().reveal('.about-wrapper, .portfolio', { delay: 500, origin: 'bottom' });
